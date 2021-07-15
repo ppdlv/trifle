5adc9692ca4bd975909fd5ffee21289047a1da18
ppdlv <szxa21@gmail.com>
2021-07-16 00:57:02 +0800

    cleaning

---
+++ lab/autoupdate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt update
+apt upgrade -y
+apt autoclean
+sed -i.backup -z "s/res === null || res === undefined || \!res || res\n\t\t\t.data.status.toLowerCase() \!== 'active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js; systemctl restart pveproxy.service
