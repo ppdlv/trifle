bcbe983f8f6e6b9148620e92a93f1b23b8f5dd70
ppdlv <szxa21@gmail.com>
2022-08-24 23:05:18 +0800

    wget instead

--- zsh/install.sh
+++ zsh/install.sh
@@ -8,13 +8,13 @@ mkdir1=$(mkdir -p /usr/share/zsh-autosuggestions)
 mkdir2=$(mkdir -p /usr/share/zsh-syntax-highlighting)
 curl0=$(curl -O https://raw.githubusercontent.com/ppdlv/trifle/main/zsh/.zshrc -o $(pwd))
 curl1=$(curl -o /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh -O https://raw.githubusercontent.com/ppdlv/trifle/main/zsh/zsh-autosuggestions.zsh)
-curl2=$(curl -o /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zip -O https://raw.githubusercontent.com/ppdlv/trifle/main/zsh/zsh-syntax-highlighting.zip)
-unzip=$(unzip -o /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zip -d /usr/share/zsh-syntax-highlighting)
-removezip=$(rm -f /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zip)
-
-$curl0
+wget=$(wget -P /usr/share/zsh-syntax-highlighting https://raw.githubusercontent.com/ppdlv/trifle/main/zsh/zsh-syntax-highlighting.zip)
+removezip=$(rm -f )
 $mkdir1
 $mkdir2
-$unzip
-$removezip
+$curl0
+$curl1
+$wget
+unzip -o /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zip -d /usr/share/zsh-syntax-highlighting
+rm -f /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zip
 chsh -s /bin/zsh
