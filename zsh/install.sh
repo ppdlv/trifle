5dc058dcbe4ae8ab07d146a73f82d69f7c1af247
ppdlv <szxa21@gmail.com>
2022-08-24 22:18:46 +0800

    fix up

--- zsh/install.sh
+++ zsh/install.sh
@@ -9,11 +9,12 @@ mkdir2=$(mkdir -p /usr/share/zsh-syntax-highlighting)
 curl0=$(curl -O https://raw.githubusercontent.com/ppdlv/trifle/main/zsh/.zshrc -o $(pwd))
 curl1=$(curl -o /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh -O https://raw.githubusercontent.com/ppdlv/trifle/main/zsh/zsh-autosuggestions.zsh)
 curl2=$(curl -o /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zip -O https://raw.githubusercontent.com/ppdlv/trifle/main/zsh/zsh-syntax-highlighting.zip)
-unzip=$(unzip /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zip -d /usr/share/zsh-syntax-highlighting)
+unzip=$(unzip -o /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zip -d /usr/share/zsh-syntax-highlighting)
 removezip=$(rm -f /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zip)
 
 $curl0
 $mkdir1
-$mkdir2 
+$mkdir2
 $unzip
+$removezip
 chsh -s /bin/zsh
