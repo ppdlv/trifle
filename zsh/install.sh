f4ecbf7f3d16478b4e5a1ca9d3d7a082933ae27e
ppdlv <szxa21@gmail.com>
2022-08-25 16:00:51 +0800

    added exit shell

--- zsh/install.sh
+++ zsh/install.sh
@@ -10,6 +10,7 @@ curl0=$(curl -O https://raw.githubusercontent.com/ppdlv/trifle/main/zsh/.zshrc -
 curl1=$(curl -o /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh -O https://raw.githubusercontent.com/ppdlv/trifle/main/zsh/zsh-autosuggestions.zsh)
 wget=$(wget -P /usr/share/zsh-syntax-highlighting https://raw.githubusercontent.com/ppdlv/trifle/main/zsh/zsh-syntax-highlighting.zip)
 removezip=$(rm -f )
+
 $mkdir1
 $mkdir2
 $curl0
@@ -18,3 +19,6 @@ $wget
 unzip -o /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zip -d /usr/share/zsh-syntax-highlighting
 rm -f /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zip
 chsh -s /bin/zsh
+
+# Exit shell
+disown -a && exit
